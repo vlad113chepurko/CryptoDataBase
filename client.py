5b390753b44132eae0674c58e1ab016f542fd1ba
vlad113chepurko <vlad113chepurko@gmail.com>
2025-03-31 16:10:47 +0300

    Added crypto, hashlib, json and server

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
+import time
 from tkinter import *
+from tkinter import messagebox
 from tkinter import ttk
-import socket
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import socket, hashlib, json
 
 # <Client>
 PATH = '127.0.0.1'
@@ -44,8 +48,8 @@ def sign_up():
     repeat_entry.grid(column=0, row=5, pady=5)
 
     # <Submit>
-    submit = ttk.Button(frm, text="Submit", command=sign_up)
-    submit.grid(column=0, row=6, pady=15)
+    submit_button = ttk.Button(frm, text="Submit", command=lambda:submit(login_entry.get(), password_entry.get(), repeat_entry.get()))
+    submit_button.grid(column=0, row=6, pady=15)
 
     # <Switch to login>
     SignIn = ttk.Button(frm, text="Sign in", command=sign_in)
@@ -86,6 +90,38 @@ def clear_frame():
     for widget in frm.winfo_children():
         widget.destroy()
 
+def show_message(state, message):
+    if state:
+        messagebox.showinfo('Completed 😀', f'{message}')
+    else:
+        messagebox.showerror('Error 😈', f'{message}')
+
+def encrypted_data(key, data):
+    cipher = AES.new(key, AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(data.encode('utf-8'))
+    data_to_send = {'key': key.hex(), 'nonce': nonce.hex(), 'ciphertext': ciphertext.hex()}
+
+    client.send(json.dumps(data_to_send).encode())
+
+def hashed(login, password):
+    key = get_random_bytes(16)
+    data = login + password
+    hashed_key = hashlib.sha256(key).digest()[:16]
+    encrypted_data(hashed_key, data)
+
+def submit(login, password, repeat):
+
+    if login and password != '':
+        if password != repeat:
+            show_message(False, 'Passwords do not match!')
+        else:
+            show_message(True, 'Data was sent to server!')
+            hashed(login, password)
+    else:
+        show_message(False, 'You must write login, password and repeat password!')
+
+
 # <Content>
 login_label = ttk.Label(frm, text='Login:', font='Arial 12')
 login_label.grid(column=0, row=0, sticky="w", pady=5)
@@ -102,11 +138,14 @@ repeat_label.grid(column=0, row=4, sticky="w", pady=5)
 repeat_entry = ttk.Entry(frm, font='Arial 12', width=30, show="*")
 repeat_entry.grid(column=0, row=5, pady=5)
 
-SignIn = ttk.Button(frm, text="Sign in", command=lambda: print("Sign In"))
+SignIn = ttk.Button(frm, text="Sign in", command=lambda:submit(login_entry.get(), password_entry.get(), repeat_entry.get()))
 SignIn.grid(column=0, row=6, pady=15)
 
 SignUp = ttk.Button(frm, text="Sign up", command=sign_up)
 SignUp.grid(column=0, row=7, pady=15)
+
+# test = ttk.Button(frm, command=lambda:show_message('Error'))
+# test.grid(column=0, row=8)
 
 root.mainloop()
 client.close()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-import socket
+import socket, json
+from Crypto.Cipher import AES
 
 PATH = '127.0.0.1'
 PORT = 12345
@@ -12,4 +13,28 @@ print("Server is running...")
 conn, addr = server.accept()
 print(f"Connection from {addr} has been established!")
 
+def handle_decrypted(key, nonce, ciphertext):
+    cipher_dec = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    decrypted_data = cipher_dec.decrypt(ciphertext).decode('utf-8')
+    print(f"Decrypted: {decrypted_data}")
+
+def handle_received_data(data):
+    received_data = json.loads(data.decode())
+    key = bytes.fromhex(received_data['key'])
+    nonce = bytes.fromhex(received_data['nonce'])
+    ciphertext = bytes.fromhex(received_data['ciphertext'])
+
+    handle_decrypted(key, nonce, ciphertext)
+
+while True:
+
+    data = conn.recv(1024)
+
+    if not data:
+        print("Server is closing...")
+        break
+
+    handle_received_data(data)
+
+conn.close()
 server.close()
